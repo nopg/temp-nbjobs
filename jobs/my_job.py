e76e60f54e549da1fe6b6ecc137a97a43729ee99
Ryan Gillespie <24619595+nopg@users.noreply.github.com>
2024-12-17 12:01:57 -0700

    initial

---
+++ jobs/my_job.py
@@ -0,0 +1,14 @@
+from nautobot.apps.jobs import Job, register_jobs, FileVar
+
+class ImportLocations(Job):
+    class metadata(self):
+        name = "Import Locations from CSV"
+        description = "RG's Jobs"
+    
+    csv = FileVar()
+
+    def run(self, csv):
+        self.logger.info("Testing...")
+
+
+register_jobs(ImportLocations)
