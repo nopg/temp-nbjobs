d55a59107489d9903e7c27f2e7aea284614d6278
Ryan Gillespie <24619595+nopg@users.noreply.github.com>
2024-12-17 13:00:58 -0700

    initial

--- jobs/load_locations.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-def load_csv(filename: str) -> dict:
-    locations = csv.DictReader(filename)
-
-def main(logger, filename) -> None:
-    locations = load_csv(filename=filename)
-    logger.info(f"locations = {locations}")
---
+++ jobs/utils.py
@@ -0,0 +1,98 @@
+import csv
+from nautobot.dcim.models import Location, LocationType
+
+STATES = {
+    'AL': 'Alabama',
+    'AK': 'Alaska',
+    'AZ': 'Arizona',
+    'AR': 'Arkansas',
+    'CA': 'California',
+    'CO': 'Colorado',
+    'CT': 'Connecticut',
+    'DE': 'Delaware',
+    'DC': 'District of Columbia',
+    'FL': 'Florida',
+    'GA': 'Georgia',
+    'HI': 'Hawaii',
+    'ID': 'Idaho',
+    'IL': 'Illinois',
+    'IN': 'Indiana',
+    'IA': 'Iowa',
+    'KS': 'Kansas',
+    'KY': 'Kentucky',
+    'LA': 'Louisiana',
+    'ME': 'Maine',
+    'MD': 'Maryland',
+    'MA': 'Massachusetts',
+    'MI': 'Michigan',
+    'MN': 'Minnesota',
+    'MS': 'Mississippi',
+    'MO': 'Missouri',
+    'MT': 'Montana',
+    'NE': 'Nebraska',
+    'NV': 'Nevada',
+    'NH': 'New Hampshire',
+    'NJ': 'New Jersey',
+    'NM': 'New Mexico',
+    'NY': 'New York',
+    'NC': 'North Carolina',
+    'ND': 'North Dakota',
+    'OH': 'Ohio',
+    'OK': 'Oklahoma',
+    'OR': 'Oregon',
+    'PA': 'Pennsylvania',
+    'RI': 'Rhode Island',
+    'SC': 'South Carolina',
+    'SD': 'South Dakota',
+    'TN': 'Tennessee',
+    'TX': 'Texas',
+    'UT': 'Utah',
+    'VT': 'Vermont',
+    'VA': 'Virginia',
+    'WA': 'Washington',
+    'WV': 'West Virginia',
+    'WI': 'Wisconsin',
+    'WY': 'Wyoming'
+}
+
+def load_csv(filename):
+    try:
+        locations = csv.DictReader(open(filename, "r"))
+        return list(locations)
+    except FileNotFoundError:
+        raise Exception(f"{filename} not found!")
+
+def create_state(name):
+    state, created = Location.objects.get_or_create(name=state, location_type=LocationType.objects.get(name="State"))
+    return created
+
+def load_locations(logger, locations):
+    for location in locations:
+        state = location.get("state")
+        if not state:
+            raise Exception(f"State missing for location: '{location.get('name')}'")
+        if state in STATES:
+            state_name = STATES[state]
+        elif state in STATES.values():
+            state_name = state
+        else:
+            raise Exception(f"State {state} does not appear to be correct.")
+        created = create_state(state_name)
+        if created:
+            logger.info(f"Created State: '{state_name}")
+
+def main(logger, filename) -> None:
+
+    locations = load_csv(filename=filename)
+    logger.info(f"locations = {locations}")
+
+    load_locations(logger, locations)
+
+
+if __name__ == "__main__":
+    import logging
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.DEBUG)
+
+    filename = "locations.csv"
+    main(logger=logger, filename=filename)
